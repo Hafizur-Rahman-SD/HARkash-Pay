e946de3e9c2452d47526e9f0976543a12f2e9c7e
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-02-25 06:57:24 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  server: {
-    port: 5173
-  }
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "src/pages/login.html"),
+        home: resolve(__dirname, "src/pages/home.html"),
+        addMoney: resolve(__dirname, "src/pages/add-money.html"),
+        cashout: resolve(__dirname, "src/pages/cashout.html"),
+        transfer: resolve(__dirname, "src/pages/transfer.html"),
+        payBill: resolve(__dirname, "src/pages/pay-bill.html"),
+        bonus: resolve(__dirname, "src/pages/bonus.html"),
+        transactions: resolve(__dirname, "src/pages/transactions.html"),
+      },
+    },
+  },
+});
